3762339ab815eeb4a9bec14eb31df06293668f7a
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-07-08 18:30:53 -0400

    updated config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000/",
-        secure: false
+        target: "http://localhost:8000/"
       },
     },
   },
