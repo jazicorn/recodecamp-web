1b2ca03a7d0ec283ccbde6617525cd16a6675213
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-07-08 22:00:11 -0400

    removed api from web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
+// import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
-const api = process.env.API;
+//const api = process.env.API;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  manifest: true,
-  server: {
-    proxy: {
-      "/api": {
-        target: api
-      },
-    },
-  },
+  plugins: [react(), svgr()]
 });
