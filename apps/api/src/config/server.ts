950511949f32e26bfa911dbb7c5a2dcf65608e01
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-07-08 23:04:42 -0400

    removed middleware

--- apps/api/src/config/server.ts
+++ apps/api/src/config/server.ts
@@ -2,24 +2,24 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
+//import cors from 'cors';
 
 class App {
     public app: Application;
     public port: number;
-    private corsOptions;
+    //private corsOptions;
 
     constructor(controllers, port: number) {
         this.app = express();
         this.port = port;
         this.initMiddlewares();
-        this.app.options('localhost', cors());
+        //this.app.options('localhost', cors());
         this.initControllers(controllers);
     }
 
     private initMiddlewares() {
         this.app.use(bodyParser.json());
-        this.app.use(cors());
+        //this.app.use(cors());
     }
 
     private initControllers(controllers) {
