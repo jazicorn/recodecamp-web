6360bb9b437d2fc376e292d07c60d887f9de6bed
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-07-13 19:51:40 -0400

    added testing

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import viteConfig from './vite.config'
+
+export default mergeConfig(viteConfig, defineConfig({
+  test: {
+    exclude: ['packages/template/*'],
+  },
+}))
