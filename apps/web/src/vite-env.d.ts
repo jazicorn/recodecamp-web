b94e80e039f3f6a5455e2bea2f6c01b3ebf37451
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-08-01 08:38:27 -0400

    updated vercel settings

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -3,6 +3,14 @@
 import { defineConfig } from 'vite'
 import viteConfig from './vite.config'
 
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 export default mergeConfig(viteConfig, defineConfig({
   test: {
     exclude: ['packages/template/*'],
