5cd8ae124d35f5084ff3c56b4c6435c5f6f4795b
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-08-23 17:47:10 -0400

    added comment to db file

--- apps/api/src/config/db.ts
+++ apps/api/src/config/db.ts
@@ -1,11 +1,11 @@
-// app.js
+// Node Postgres Docs: https://github.com/porsager/postgres
 const postgres = require('postgres');
 require('dotenv').config();
 
 const { API_PGHOST, API_PGDATABASE, API_PGUSER, API_PGPASSWORD, API_ENDPOINT_ID } = process.env;
 const URL = `postgres://${API_PGUSER}:${API_PGPASSWORD}@${API_PGHOST}/${API_PGDATABASE}?options=project%3D${API_ENDPOINT_ID}`;
 
-const client = postgres(URL, { ssl: 'require' });
+const sql = postgres(URL, { ssl: 'require' });
 
-export default client;
+export default sql;
 
