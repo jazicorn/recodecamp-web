2c26d19c4d4a9bfff1ddb593ed0009c6c099d695
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-08-24 18:59:45 -0400

    removed unused string route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,12 +2,11 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 import App from './config/server';
 import Index from './controllers/index.controller';
-import Comments from './controllers/comments.controller';
+import Comments from './controllers/comments/comments.controller';
 import VarGeneral from './controllers/variables/var.controller';
 import VarDeclare from './controllers/variables/var.declare.controller';
 import VarScope from './controllers/variables/var.scope.controller';
 import VarScopeReassign from './controllers/variables/var.scope.reassign.controller';
-import Strings from './controllers/strings.controller';
 
 const app = new App([
     new Index(),
@@ -15,8 +14,7 @@ const app = new App([
     new VarGeneral(),
     new VarDeclare(),
     new VarScope(),
-    new VarScopeReassign(),
-    new Strings()
+    new VarScopeReassign()
 ]);
 
 app.listen();
