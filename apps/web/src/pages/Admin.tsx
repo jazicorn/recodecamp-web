e62cc29738d987721ff64e40ffb641773160d15c
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-08-24 21:53:15 -0400

    create admin login page

--- apps/web/src/pages/Admin.tsx
+++ apps/web/src/pages/Admin.tsx
@@ -1,74 +1,39 @@
 import { useContext } from 'react';
-import { Outlet, Navigate } from 'react-router-dom';
 import { ThemeContext } from '../context/ThemeContext';
-//import { useAppSelector } from '../redux/reduxHooks.ts';
-//import type { RootState } from '../redux/store.ts'
-// hooks
-import useWindowSize from '../hooks/useWindowSize';
-//import Transition from '../hooks/useTransition';
-// components
-import Header from '../components/header/Header.Dashboard';
-import D_Navigation from '../components/dashboard/D_Navigation';
-import D_NavigationMobile from '../components/dashboard/D_NavigationMobile';
-import D_Header from '../components/dashboard/D_Header';
+import Header from '../components/header/Header';
+import Transition from '../hooks/useTransition';
 
 const prodURL = import.meta.env.PROD;
 
-const Dashboard = () => {
-  const { isDesktopMDXL, isDesktopXL } = useWindowSize();
+function Home() {
   const { state } = useContext(ThemeContext);
   const darkMode = state.darkMode;
-  //const menuItem = useAppSelector((state:RootState) => state?.dashboard?.value);
 
   return (
-    <span>
+    <div className='tw-dark'>
     {prodURL ? 
        <Navigate to="/" replace={true} />
-      :  
-      <div className="tw-font-mono">
-        {/**Background | Position: Absolute */}
-        <div className={`${ darkMode ? 'tw-bg-[url(../assets/bg/sw-1.jpg),_url(../assets/bg/landscape.jpg)] tw-bg-blend-overlay tw-opacity-40' :
-                    'tw-bg-[url(../assets/bg/sw-1.jpg),_url(../assets/bg/landscape.jpg)] tw-bg-blend-overlay tw-opacity-40'
-          } tw-fixed tw-bg-cover tw-bg-center tw-bg-no-repeat tw-h-full tw-w-full`}/>
-        {/**Page Content | Position: Relative */}
-        <article className="tw-relative tw-z-10tw-h-screen tw-w-full tw-flex tw-flex-col tw-grow tw-place-items-center">
+      :
+      <span> 
+        <div className={`${darkMode ? 'tw-bg-[url(../assets/bg/sw-1.jpg),_url(../assets/bg/landscape.jpg)] tw-bg-blend-overlay tw-bg-blend-darken tw-brightness-60': 
+        'tw-bg-[url(../assets/bg/sw-1.jpg),_url(../assets/bg/landscape.jpg)] tw-bg-blend-overlay tw-opacity-50' } tw-fixed tw-absolute tw-bg-cover tw-bg-center tw-bg-no-repeat tw-h-full tw-w-full` }/>
+        <article className="tw-relative tw-z-100 tw-bg-transparent tw-dark tw-font-mono tw-h-screen tw-w-screen tw-flex tw-flex-col tw-place-content-between ">
+          {/** Navigation */}
           <Header />
-          {isDesktopMDXL || isDesktopXL ? 
-          <main className={`${darkMode ? '[&>section]:tw-backdrop-brightness-25 ' : '[&>section]:tw-backdrop-brightness-65'} 
-            tw-bg-transparent tw-pb-5 tw-mt-1 tw-w-full tw-h-full [&>*]:tw-backdrop-blur-sm
-            tw-grid tw-grid-rows-dashboard tw-grid-cols-dashboard tw-gap-1 tw-px-5 [&>*]:tw-rounded tw-border tw-border-transparent`}>
-              <section className='tw-col-start-1 tw-col-end-1 tw-row-start-1 tw-row-end-3'>
-                <D_Navigation/>
-              </section>
-              <section className='tw-col-start-2 tw-col-end-3 tw-row-start-1 tw-row-end-2'>
-                <D_Header/>
-              </section>
-              <div className='tw-col-start-2 tw-col-end-3 tw-row-start-2 tw-row-end-3'>
-                <Outlet/>
-              </div>
-          </main>
-          :
-          <main className={`${darkMode ? '[&>section]:tw-backdrop-brightness-25 ' : '[&>section]:tw-backdrop-brightness-65'} 
-            tw-bg-transparent tw-pb-5 tw-mt-1 tw-w-full tw-h-full tw-grow [&>*]:tw-backdrop-blur-sm
-            tw-grid tw-grid-rows-dashboard-mobile tw-grid-cols-dashboard-mobile tw-gap-1 tw-px-5 [&>*]:tw-rounded tw-border tw-border-transparent`}>
-              {/** z index can't go higher than 50 to work*/}
-              <section className='tw-row-start-1 tw-row-end-1 tw-z-50'>
-                <D_NavigationMobile/>
-              </section>
-              <section className=' tw-row-start-2 tw-row-end-2'>
-                <D_Header/>
-              </section>
-              <div className='tw-row-start-3 tw-row-end-6'>
-                <Outlet/>
-              </div>
-          </main>
-          }
+          <Transition>
+            <div className="tw-flex tw-place-content-center">
+              <h1>Admin Page</h1>
+            </div>
+          </Transition>
+          {/**Footer */}
+          <footer className={`${darkMode ? 'tw-bg-campfire-neutral-800 tw-text-campfire-neutral-200' : 'tw-bg-campfire-blue-100'} tw-bg-campfire-gray tw-flex tw-flex-col tw-place-items-center tw-w-full tw-h-[28px] tw-pr-3`}>
+            <a href="https://github.com/jazicorn"className="tw-pr-2">created by Jazicorn</a>
+          </footer>
         </article>
-      </div>
+      </span>
     }
-    </span>
+    </div>
   )
 }
 
-export default Dashboard
- 
+export default Home
