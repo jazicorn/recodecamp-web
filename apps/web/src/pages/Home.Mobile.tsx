7277f03cc836107d93ff9d42e33527a38de44b29
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-09-01 14:52:52 -0400

    change background image to fixed

--- apps/web/src/pages/Home.Mobile.tsx
+++ apps/web/src/pages/Home.Mobile.tsx
@@ -10,8 +10,8 @@ function HomeMobile() {
   return (
     <div className='tw-dark'>
       <div className={`${darkMode ? 'tw-bg-[url(../assets/bg/sw-1.jpg),_url(../assets/bg/landscape.jpg)] tw-bg-blend-overlay tw-bg-blend-darken tw-brightness-60': 
-      'tw-bg-[url(../assets/bg/sw-1.jpg),_url(../assets/bg/landscape.jpg)] tw-bg-blend-overlay tw-opacity-50' } tw-absolute tw-bg-cover tw-bg-center tw-bg-no-repeat tw-h-full tw-w-full` }/>
-      <article className={`${darkMode ? '' : ''} tw-relative tw-z-100 tw-bg-transparent tw-font-mono tw-h-screen tw-w-screen tw-flex tw-flex-col tw-place-content-between`}>
+      'tw-bg-[url(../assets/bg/sw-1.jpg),_url(../assets/bg/landscape.jpg)] tw-bg-blend-overlay tw-opacity-50' } tw-fixed tw-bg-cover tw-bg-center tw-bg-no-repeat tw-h-full tw-w-full` }/>
+      <article className={`${darkMode ? '' : ''} tw-relative tw-z-100 tw-bg-transparent tw-h-screen tw-w-screen tw-flex tw-flex-col tw-place-content-between`}>
         {/** Navigation */}
         <Header />
         <div className={`${darkMode ? 'tw-border-neutral-500' : 'tw-border-neutral-800'} tw-border-y-2 tw-h-full`}>
