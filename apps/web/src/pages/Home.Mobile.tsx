b616916d657ae1b9c53a0546a0340ba56baf93b7
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-09-02 19:23:44 -0400

    added font to footer

--- apps/web/src/pages/Home.Mobile.tsx
+++ apps/web/src/pages/Home.Mobile.tsx
@@ -19,8 +19,8 @@ function HomeMobile() {
         <HomeBanner/>
         </div>
         {/**Footer */}
-        <footer className={`${darkMode ? 'tw-bg-campfire-neutral-800 tw-text-campfire-neutral-200' : 'tw-bg-campfire-blue-100'} tw-bg-campfire-gray tw-flex tw-flex-col tw-place-items-center tw-w-full tw-h-[28px] tw-pr-3`}>
-          <a href="https://github.com/jazicorn"className="tw-pr-2">created by Jazicorn</a>
+        <footer className={`${darkMode ? 'tw-bg-campfire-neutral-800 tw-text-campfire-neutral-200' : 'tw-bg-campfire-blue-100'} tw-bg-campfire-gray tw-flex tw-flex-col tw-place-items-center tw-w-full tw-h-[28px] tw-pr-3 tw-font-space_mono`}>
+          <a href="https://github.com/jazicorn" className="tw-pr-2">created by Jazicorn</a>
         </footer>
       </article>
     </div>
