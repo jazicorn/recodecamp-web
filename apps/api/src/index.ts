b0fed5e21cda8bb735bcd50cc6cd40e64c4cf3bf
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-10-02 13:55:28 -0400

    renamed controller subfolder

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,13 +3,13 @@
 import App from './server';
 import Index from './controllers/index.controller';
 import guestRoutes from './controllers/auth/guest/guest.controller';
-import jsComments from './controllers/data/javascript/comments.controller';
-import javaComments from './controllers/data/java/comments.controller';
-import pythonComments from './controllers/data/python/comments.controller';
-//import VarGeneral from './controllers/data/javascript/variables/var.controller';
-import VarDeclare from './controllers/data/javascript/variables/var.declare.controller';
-import VarScope from './controllers/data/javascript/variables/var.scope.controller';
-import VarScopeReassign from './controllers/data/javascript/variables/var.scope.reassign.controller';
+import jsComments from './controllers/questions/javascript/comments.controller';
+import javaComments from './controllers/questions/java/comments.controller';
+import pythonComments from './controllers/questions/python/comments.controller';
+//import VarGeneral from './controllers/questions/javascript/variables/var.controller';
+import VarDeclare from './controllers/questions/javascript/variables/var.declare.controller';
+import VarScope from './controllers/questions/javascript/variables/var.scope.controller';
+import VarScopeReassign from './controllers/questions/javascript/variables/var.scope.reassign.controller';
 
 const app = new App(
     [],
