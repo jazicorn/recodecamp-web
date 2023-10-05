59585f5e5f46141897ef6ae344620ff48d3e0765
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-10-05 00:41:06 -0400

    updated header options

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -31,13 +31,13 @@ class App {
         this.app.use(cors({
             origin: this.corsOptions
         }));
-        this.app.options("/", (req, res) => {
+        this.app.options("*", (req, res) => {
             res.setHeader("Access-Control-Allow-Origin", "*");
             res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
             res.setHeader("Access-Control-Allow-Headers", "Content-Type");
             res.sendStatus(204);
         });
-        this.app.use( (req, res, next) => {
+        this.app.use( "*", (req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
             res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
