ec5f7cd310220dd2080ae6a6d6be312d30e17e2e
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-11-14 19:36:09 -0500

    removed unused object

--- src/utils/constants/constLanguages.ts
+++ src/utils/constants/constLanguages.ts
@@ -7,30 +7,6 @@ import { javascript } from '@codemirror/lang-javascript';
 import { java } from '@codemirror/lang-java';
 import { python } from '@codemirror/lang-python';
 
-export const DEFAULT_USER = {
-  _ID: '123-456-789',
-  _CREATED_AT: new Date().toISOString(),
-  _UPDATED_AT: new Date().toISOString(),
-  _ACCESS_TOKEN: '',
-  _FIRST_LOGIN: false,
-  _ADMIN: false,
-  _SUBSCRIPTION: '1000, Unknown',
-  _IP_ADDRESS: '',
-  _PASSCODE: '',
-  _PASSCODE_CONFIRMED: false,
-  _EMAIL: '',
-  _EMAIL_CONFIRMED: false,
-  _EMAIL_PASSCODE: '',
-  _PASSWORD: '',
-  _DEFAULT_LANGUAGE: '',
-  _DEFAULT_ROUTE: '',
-  _POINTS_TOTAL: 0,
-  _POINTS_JAVASCRIPT: 0,
-  _POINTS_JAVA: 0,
-  _POINTS_PYTHON: 0,
-  _COURSES: '',
-};
-
 export const _LANGUAGES_SHORTHAND = {
   javascript: 'js',
   java: 'java',
