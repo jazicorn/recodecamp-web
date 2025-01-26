0762043ddc1787151a1c15418b5b21e3dd64401e
jazicorn <40859840+jazicorn@users.noreply.github.com>
2025-01-26 10:19:52 -0500

    updated route name

--- src/utils/constants/constDashboardRoutes.ts
+++ src/utils/constants/constDashboardRoutes.ts
@@ -1,27 +1,15 @@
 /** API url | Custom env mandatory to begin with VITE
  * https://vitejs.dev/guide/env-and-mode.html#env-files */
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const apiURL = import.meta.env.VITE_API_BASE_URL;
 
 export const _QUESTION_ROUTE = (currentLanguage, getMenuRoute) => {
-    if (import.meta.env.PROD) {
-        return `${baseURL}/api/${currentLanguage}/${getMenuRoute}`
-    } else {
-        return `/api/${currentLanguage}/${getMenuRoute}`
-    }
+    return `${apiURL}/api/${currentLanguage}/${getMenuRoute}`
 };
 
 export const _CATEGORIES_ROUTE = (currentLanguage) => {
-    if (import.meta.env.PROD) {
-        return `${baseURL}/api/categories/${currentLanguage}`
-    } else {
-        return `api/categories/${currentLanguage}`
-    }
+    return `${apiURL}/api/categories/${currentLanguage}`
 };
 
 export const _LANGUAGES_ROUTE = () => {
-    if (import.meta.env.PROD) {
-        return `${baseURL}/api/languages`
-    } else {
-        return `/api/languages`
-    }
+    return `${apiURL}/api/languages`
 };
